40d78216129ff2c5b95b9ea310575a10d5abd42f
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-03-05 11:33:20 -0300

    refactor: DeleteProductController

--- src/application/presentation/controllers/deleteProductController.ts
+++ src/application/presentation/controllers/deleteProductController.ts
@@ -9,11 +9,11 @@ import {
 } from '@/core/ports/driving/presentation'
 
 export class DeleteProductController implements IController {
-  constructor (private readonly deleteProduct: IDeleteProduct) { }
+  constructor (private readonly _deleteProduct: IDeleteProduct) { }
   async handle (request: any): Promise<IHTTPResponse> {
     try {
       const { id } = request.params
-      await this.deleteProduct.delete(id)
+      await this._deleteProduct.delete(id)
       return noContent()
     } catch (error) {
       return serverError(error)
