b2689b6d1ba5e99cb398ae8b69c67dd2298bb6d6
Gabriel Silva <gabrielsilva.oliv20@gmail.com>
2024-03-10 15:55:09 -0300

    chore: new iac - eks workload

--- infra-tf/7 - ansible.tf
+++
@@ -1,7 +0,0 @@
-resource "null_resource" "ansible" {
-    depends_on = [aws_instance.fiap]
-
-    provisioner "local-exec" {
-        command = "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i ansible/inventory.ini ansible/instance.yml"
-    }
-}
