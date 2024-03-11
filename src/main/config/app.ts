2d7ea8776394061478fd58da3eb677fbeb47d813
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-03-11 01:03:42 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express, { type Express } from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupSwagger from './configSwagger'
 
 export const setupApp = (): Express => {
   const app = express()
+  setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app)
   return app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: '15s Snacks API',
+    description: 'FIAP Tech Challenge Backend',
+    version: '1.0.0'
+  }
+}
