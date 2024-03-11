8aace87d818e3ab091d32e30a2b0548d96bd23d5
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-03-11 20:17:19 -0300

    refactor: remove unused payment layer

--- src/main/factories/controllers/makePaymentNotificationController.ts
+++
@@ -1,8 +0,0 @@
-import { makeLogControllerDecorator } from '@/main/factories/decorators'
-import { PaymentNotificationController } from '@/application/presentation/controllers'
-import { type IController } from '@/application/presentation/ports'
-
-export const makePaymentNotificationController = (): IController => {
-  const controller = new PaymentNotificationController()
-  return makeLogControllerDecorator(controller)
-}
